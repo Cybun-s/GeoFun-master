ec93f9c41354d102301bf5c2a09e7736258f10c3
githuber8 <Julian1.Kammermeier@stud.uni-regensburg.de>
2020-10-03 18:32:30 +0200

    Prepared for sharedPreferences and Widget

--- app/src/main/java/com/excercise/geofun/ActivitySettings.java
+++ app/src/main/java/com/excercise/geofun/ActivitySettings.java
@@ -14,20 +14,40 @@ public class ActivitySettings extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        returnToPreviousPage();
-
-    }
-
-    private void returnToPreviousPage(){
         Button settingsBackButton = findViewById(R.id.settings_back);
+        Button locationSetting = findViewById(R.id.locationSwitch);
+        Button notificationSetting = findViewById(R.id.notificationSwitch);
+
         settingsBackButton.setOnClickListener(this);
+        locationSetting.setOnClickListener(this);
+        notificationSetting.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.settings_back:
+                returnToMainPage();
+                break;
+            case R.id.locationSwitch:
+                changeLocationSetting();
+                break;
+            case R.id.notificationSwitch:
+                changeNotificationSetting();
+                break;
+        }
+    }
+
+    public void returnToMainPage(){
         Intent intentBack = new Intent(ActivitySettings.this, MainActivity.class);
         startActivity(intentBack);
     }
 
+    public void changeLocationSetting(){
     }
+
+    public void changeNotificationSetting(){
+    }
+
+}
 
---
+++ app/src/main/java/com/excercise/geofun/geoFunWidgetProvider.java
@@ -0,0 +1,6 @@
+package com.excercise.geofun;
+
+import android.appwidget.AppWidgetProvider;
+
+public class geoFunWidgetProvider extends AppWidgetProvider {
+}
