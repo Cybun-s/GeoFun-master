c1a924c028a3a49a75e04612869dc273c4df281c
Cybun-s <bitteralwina@yahoo.de>
2020-10-12 01:19:08 +0200

    Added Location Permission Request

--- app/src/main/java/com/excercise/geofun/ActivitySettings.java
+++ app/src/main/java/com/excercise/geofun/ActivitySettings.java
@@ -1,15 +1,22 @@
 package com.excercise.geofun;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class ActivitySettings extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int LOCATION_ALLOWED_CODE = 1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +51,41 @@ public class ActivitySettings extends AppCompatActivity implements View.OnClickL
     }
 
     public void changeLocationSetting(){
+        if (ContextCompat.checkSelfPermission(ActivitySettings.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(ActivitySettings.this, "Standortzugriff wurde bereits erteilt", Toast.LENGTH_SHORT).show();
+        } else {
+            getLocationPermission();
+        }
     }
+
+
+    private void getLocationPermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Standortfreigabe")
+                    .setMessage("Um alle Funktionen von GeoFun nutzen zu können, geben Sie den Standort frei.")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(ActivitySettings.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_ALLOWED_CODE);
+                        }
+                    })
+                    .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_ALLOWED_CODE);
+        }
+
+    }
+
 
     public void changeNotificationSetting(){
     }
